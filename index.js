bcea6d032544517806c800874536a8eb99d3440f
gopalaxdev <agrawalgopala@gmail.com>
2024-07-11 01:52:59 +0530

    fix: build

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
-const express = require('express')
-const path = require('path')
-const opn = require('opn')
+const express = require("express");
+const path = require("path");
+const opn = require("opn");
 
-const server = express()
-const host = 'http://localhost:8082'
-server.use('/assets', express.static(path.resolve(__dirname, './assets')))
-server.use('/dist', express.static(path.resolve(__dirname, './dist')))
+const server = express();
+const host = "http://localhost:8082";
+server.use("/assets", express.static(path.resolve(__dirname, "./assets")));
+server.use("/dist", express.static(path.resolve(__dirname, "./dist")));
 
-server.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, './index.html'));
-})
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "dist", "index.html"));
+});
 
 server.listen(8082, () => {
-  console.log(`server started at ${host}`)
-  opn(host)
-})
+  console.log(`server started at ${host}`);
+  opn(host);
+});
